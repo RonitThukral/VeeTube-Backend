4fd01ee50c68df8bb3ffdd9105f696d8017c2af7
Ronit <sarikaarora3397@gmail.com>
2024-08-13 19:00:19 +0530

    Using jwt and bcrypt and making mongoose plugins for mongoose aggregate paginate and mongoose methods

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -57,5 +57,47 @@ const userSchema = new Schema({
 }
 )
 
+userSchema.pre("save", async function (next) {
+
+    if (!this.isModified(this.password)) return next()
+
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = function (password) {
+    return bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateJwtToken = async function ()  {
+    jwt.sign(
+    {
+        _id : this._id,
+        email : this.email,
+        userName : this.userName,
+        fullName : this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+    
+)
+}
+
+userSchema.methods.generateRefreshToken = async function () {
+   return jwt.sign(
+        {
+            _id : this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
 
 export const User = mongoose.model("User", userSchema)
